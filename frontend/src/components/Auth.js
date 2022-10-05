6cc0807082138800eaf3be4036ef6b66d6837336
John-v77 <john.vs11@hotmail.com>
2022-10-05 16:51:33 -0400

    fix user display

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,36 +1,33 @@
-import { GoogleLogin } from 'react-google-login'
-import actions from '../api'
+import { GoogleLogin } from 'react-google-login';
+import actions from '../api';
 import TheContext from '../TheContext';
-import {useContext} from 'react'
-
-console.log(process.env)
+import { useContext } from 'react';
 
 const Auth = (props) => {
-    const { user, history, setUser } = useContext(TheContext)
-    const onResponse = (response) => {
-        console.log(response)
-        actions
-            .logIn(response)
-            .then(res => {
-                console.log(res)
-                setUser(res.data)
-                history.push('/')
+  const { user, history, setUser } = useContext(TheContext);
+  const onResponse = (response) => {
+    console.log(response);
+    actions
+      .logIn(response)
+      .then((res) => {
+        console.log(res);
+        setUser(res.data);
+        history.push('/');
+      })
+      .catch(console.error);
+  };
 
-            })
-            .catch(console.error)
-    }
+  return (
+    <div>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLEID}
+        buttonText='Signup'
+        onSuccess={onResponse}
+        onFailure={onResponse}
+        cookiePolicy={'single_host_origin'}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div>
-        <GoogleLogin
-            clientId={process.env.REACT_APP_GOOGLEID}
-            buttonText="Signup"
-            onSuccess={onResponse}
-            onFailure={onResponse}
-            cookiePolicy={"single_host_origin"}
-        />
-        </div>
-    );
-}
-
-export default Auth
+export default Auth;
